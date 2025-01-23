d7679aa8adc59bb84cc8949707ab79b51c2c582e
Diego Ysrael Antonio <https://github.com/DiegoAntonio04/Antonio_DSAMidterms.git>
2025-01-23 22:22:06 +0800

    fixed some syntax

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ app.use('/submitFave', submit);
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {    
-console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
