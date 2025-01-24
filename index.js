8e4396a4f02763beb924a3863a88eccff0cfb16a
Diego Ysrael Antonio <https://github.com/DiegoAntonio04/Antonio_DSAMidterms.git>
2025-01-24 18:04:42 +0800

    removed some code

--- index.js
+++ index.js
@@ -68,9 +68,6 @@ app.use('/submit', submit);
 
 const PORT = process.env.PORT ||3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 server.listen(PORT, () => {    
     console.log(`Server is running on port ${PORT}`);
