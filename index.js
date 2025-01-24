6bb6dac631a6bf7dc899ae9e67ce0eb7219679be
Diego Ysrael Antonio <https://github.com/DiegoAntonio04/Antonio_DSAMidterms.git>
2025-01-24 16:54:44 +0800

    added syntax

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ app.use('/submit', submit);
 
 const PORT = process.env.PORT ||3000
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 server.listen(PORT, () => {    
     console.log(`Server is running on port ${PORT}`);
 });
