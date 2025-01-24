6b46997c30655d9e9c808c0d5402712fc7447012
Diego Ysrael Antonio <https://github.com/DiegoAntonio04/Antonio_DSAMidterms.git>
2025-01-24 16:23:14 +0800

    fixed syntax error

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.use('/submit', submit);
 
 const PORT = process.env.PORT ||3000
 
-app.listen(PORT, () => {    
+server.listen(PORT, () => {    
     console.log(`Server is running on port ${PORT}`);
 });
